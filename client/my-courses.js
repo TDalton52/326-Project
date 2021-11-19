8d715fe39ef2f8198a2000bdc05a229066a180bd
Tim Dalton <tdalton579@gmail.com>
2021-11-19 18:06:15 -0500

    fixed minor bug in my-courses

--- client/my-courses.js
+++ client/my-courses.js
@@ -3,19 +3,19 @@
 window.addEventListener("load", async function()
 {
   //TODO: edit URL so that the proper user is fetched
-  const response = await fetch(`http://localhost:3000/getData/user`, {headers:{"accepts":"application/json"}});
-  const data = await response.json();
-  console.log(JSON.stringify(data));
-  for(let index in data)
+  //const response = await fetch(`http://localhost:3000/getData/user`, {headers:{"accepts":"application/json"}});
+  //const data = await response.json();
+  const data = [{"class-name":"COMPSCI 326","school":"UMass","instructor":"Emery Berger","time":"1:00 - 2:15"}];
+  for(const index in data)
   {
-    console.log(JSON.stringify(data[index]));
+    console.log(index);
     const newRow = document.createElement("div");
     newRow.classList.add("row");
-    for(const key in data)
+    for(const key in data[index])
     {
       const newElem = document.createElement("span");
       newElem.classList.add(key);
-      newElem.innerText = data[key];
+      newElem.innerText = data[index][key];
       newRow.appendChild(newElem);
     }
     document.getElementById("container").appendChild(newRow);

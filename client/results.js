998f21902f4152631c19dd1a90a25b86842c0210
Tim Dalton <tdalton579@gmail.com>
2021-11-24 18:53:42 -0500

    home button for results.js now works

--- client/results.js
+++ client/results.js
@@ -3,12 +3,7 @@
 //Add links to buttons: these lines find all buttons that link to a specific page and add that event listener
 if(document.querySelector(".homebutton") !== null)
 {
-  document.querySelector(".homebutton").addEventListener("click", function() {window.location.href = "http://localhost:8000/home.html";});
-}
-
-if(document.getElementById("searchresults") !== null)
-{
-  document.getElementById("searchresults").addEventListener("click", function() {window.location.href = "http://localhost:8000/results.html";});
+  document.querySelector(".homebutton").addEventListener("click", function() {window.location.href =window.location.href = `https://${window.location.hostname}/client/home.html`;});
 }
 
 window.addEventListener("load", function()

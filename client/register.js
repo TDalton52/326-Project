37d233d6429cd86bdb595343058f77e096943e6a
Tim Dalton <tdalton579@gmail.com>
2021-11-24 21:45:28 -0500

    added register.js

---
+++ client/register.js
@@ -0,0 +1,15 @@
+document.getElementById("login").addEventListener("click", async function(){
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const response = await fetch('/register', {
+      method: 'POST',
+      body: JSON.stringify({
+          name: username,
+          pwd: password,
+      }),
+      headers: { 'Content-Type' : 'application/json'}
+  });
+  if (!response.ok) {
+      console.error("I fucked up.");
+  }
+});

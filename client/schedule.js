bdc077c1433e35d513bbd53bedd9bd976c0e3736
Tim Dalton <tdalton579@gmail.com>
2021-12-11 16:57:02 -0500

    first draft of parsing out time values from course data, many bugfixes soon to come

--- client/schedule.js
+++ client/schedule.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { handle } = require("express/lib/application");
+
 //Add links to buttons: these lines find all buttons that link to a specific page and add that event listener
 document.getElementById("homebutton").addEventListener("click", function() {window.location.href = `https://${window.location.hostname}`;});
 document.getElementById("searchbutton").addEventListener("click", function() {window.location.href = `https://${window.location.hostname}/client/results.html`;});
@@ -17,23 +19,203 @@ function detectConflict(data)
   }
 }
 
-window.addEventListener("load", function()
+handleHolyokeAndAmherstDaySystem(dayString, startTime, endTime)
 {
-  if(!("courses" in window.localStorage))
+  if(startTime.charAt(0) == "0")
   {
-    window.localStorage.setItem("courses", JSON.stringify([]))
+    startTime = startTime.substring(1);
   }
-  const data = JSON.parse(window.localStorage.getItem("courses"));
+  if(endTime.charAt(0) == "0")
+  {
+    endTime = endTime.substring(1);
+  }
+  if(dayString == "M")
+  {
+    document.getElementById(`M${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`M${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+  else if(dayString == "T")
+  {
+    document.getElementById(`TU${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`TU${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+  else if(dayString == "W")
+  {
+    document.getElementById(`W${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`W${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+  else if(dayString == "TH")
+  {
+    document.getElementById(`TH${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`TH${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+  else if(dayString == "F")
+  {
+    document.getElementById(`F${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`F${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+  else if(dayString == "MW")
+  {
+    document.getElementById(`M${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`M${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`W${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`W${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+  else if(dayString == "MF")
+  {
+    document.getElementById(`M${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`M${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`F${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`F${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+  else if(dayString == "WF")
+  {
+    document.getElementById(`W${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`W${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`F${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`F${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+  else if(dayString == "MWF")
+  {
+    document.getElementById(`M${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`M${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`W${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`W${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`F${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`F${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+  else if(dayString == "TTH")
+  {
+    document.getElementById(`TU${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`TU${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`TH${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`TH${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+}
+
+handleUntimed(course)
+{
+  if(document.getElementById("notime").innerText == "")
+  {
+    document.getElementById("notime").innerText = course.name;
+  }
+  else
+  {
+    document.getElementById("notime").innerText += ", " + course.name;
+  }
+}
+
+handleUmass(course)
+{
+  const timeInfo = course.time.split(" ");
+  const endTime = timeInfo.pop();
+  const startTime = timeInfo.pop();
+  if(timeInfo.length == 0)
+  {
+    handleUntimed(course);
+    return;
+  }
+  for(const day in timeInfo) //last 2 elements are time, popping them out will give just days
+  {
+    document.getElementById(`${day}${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`${day}${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+}
+
+handleHolyoke(course)
+{
+  const timeInfo = course.time.split(" ");
+  const startTime = timeInfo[1];
+  const endTime = timeInfo[2].split("-")[1];
+  handleHolyokeAndAmherstDaySystem(timeInfo[0], startTime, endTime);
+}
+
+handleAmherst(course)
+{
+  const timeInfo = course.time.split(" ");
+  const times = timeInfo[1].split("-");
+  if(times.length == 0)
+  {
+    handleUntimed(course);
+    return;
+  }
+  const startTime = times[0];
+  const endTime = times[1];
+  handleHolyokeAndAmherstDaySystem(timeInfo[0], startTime, endTime);
+}
+
+handleSmith(course)
+{
+  const timeInfo = course.time.split(" ");
+  timeInfo.pop();
+  const endTime = timeInfo.pop();
+  timeInfo.pop();
+  timeInfo.pop();
+  const startTime = timeInfo.pop();
+  for(const day in timeInfo) //last 5 elements are AM/PM, a time, "-", AM/PM, and another time in that order, popping them out will give just days
+  {
+    document.getElementById(`${day}${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`${day}${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+}
+
+handleHampshire(course)
+{
+  if(course.time == "#NAME?") //why does this happen?
+  {
+    handleUntimed(course);
+    return;
+  }
+  const timeInfo = course.time.split(";");
+  for(const entry in timeInfo)
+  {
+    const day = entry.split(" ");
+    const times = day[0].split("-");
+    const startTime = times[0].substring(0, 5);
+    const endTime = times[0].substring(0, 5);
+    if(startTime.charAt(0) == "0")
+    {
+      startTime = startTime.substring(1);
+    }
+    if(endTime.charAt(0) == "0")
+    {
+      endTime = endTime.substring(1);
+    }
+    document.getElementById(`${day[1]}${startTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+    document.getElementById(`${day[1]}${endTime.charAt(0)}`).innerText += course.name + " (" + course.time + ")";
+  }
+}
+
+window.addEventListener("load", async function()
+{
+  const response = await fetch(`https://${window.location.hostname}/myCourses`);
+  const data = await response.json();
   detectConflict(data);
   for(const index in data)
   {
-    const days = data[index].time.split(" ")[0].split(/(?=[A-Z])/); //split by any capital letter
-    const startTime = data[index].time.split(" ")[1].split("-")[0];
-    const endTime = data[index].time.split(" ")[1].split("-")[1];
-    for(const day in days)
+    if(data[index].time == null)
     {
-      document.getElementById(`${days[day]}${startTime.charAt(0)}`).innerText += data[index].name + " (" + data[index].time + ")";
-      document.getElementById(`${days[day]}${endTime.charAt(0)}`).innerText += data[index].name + " (" + data[index].time + ")";
+      handleUntimed(data[index]);
+    }
+    else if(data[index].school == "UMass Amherst")
+    {
+      handleUmass(data[index]);
+    }
+    else if(data[index].school == "Mount Holyoke College")
+    {
+      handleHolyoke(data[index]);
+    }
+    else if(data[index].school == "Amherst College")
+    {
+      handleAmherst(data[index]);
+    }
+    else if(data[index].school == "Smith College")
+    {
+      handleSmith(data[index]);
+    }
+    else if(data[index].school == "Hampshire College")
+    {
+      handleHampshire(data[index]);
     }
   }
 });

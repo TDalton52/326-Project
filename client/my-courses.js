233ec0de9904d723b3265bf824b70f2b4a1164e6
Tim Dalton <tdalton579@gmail.com>
2021-12-11 20:20:29 -0500

    fixed floating delete button

--- client/my-courses.js
+++ client/my-courses.js
@@ -19,31 +19,37 @@ window.addEventListener("load", async function()
       newElem.innerText = data[index][key];
       newRow.appendChild(newElem);
     }
-    const newButton = document.createElement("button");
-    newButton.innerText = "Delete"
-    newButton.classList.add("btn");
-    newButton.classList.add("btn-primary")
-    newButton.addEventListener("click", async function()
+    if(index != "0")
     {
-      const response = await fetch(`https://${window.location.hostname}/deleteCourse`, {
-        method:"POST",
-        headers:{
-          "Accept": "application/json",
-          "Content-Type": "application/json"
-        },
-        body:JSON.stringify(data[index])
+      const newButtonContainer = document.createElement("span");
+      newButtonContainer.classList.add("buttoncontainer");
+      const newButton = document.createElement("button");
+      newButton.innerText = "Delete"
+      newButton.classList.add("btn");
+      newButton.classList.add("btn-primary")
+      newButton.addEventListener("click", async function()
+      {
+        const response = await fetch(`https://${window.location.hostname}/deleteCourse`, {
+          method:"POST",
+          headers:{
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+          },
+          body:JSON.stringify(data[index])
+        });
+        if(!response.ok)
+        {
+          console.log(response.error);
+          return;
+        }
+        else
+        {
+          console.log("POST sent successfully!");
+        }
       });
-      if(!response.ok)
-      {
-        console.log(response.error);
-        return;
-      }
-      else
-      {
-        console.log("POST sent successfully!");
-      }
-    });
-    newRow.appendChild(newButton);
+      newButtonContainer.appendChild(newButton);
+      newRow.appendChild(newButtonContainer);
+    }
     document.getElementById("container").appendChild(newRow);
   }
 });

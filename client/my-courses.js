84c9139f3adab1c9242216a276021232870ced3a
Tim Dalton <tdalton579@gmail.com>
2021-12-11 20:38:39 -0500

    bugfixing schedule

--- client/my-courses.js
+++ client/my-courses.js
@@ -20,37 +20,34 @@ window.addEventListener("load", async function()
       newElem.innerText = data[index][key];
       newRow.appendChild(newElem);
     }
-    if(index != "0")
+    const newButtonContainer = document.createElement("span");
+    newButtonContainer.classList.add("buttoncontainer");
+    const newButton = document.createElement("button");
+    newButton.innerText = "Delete"
+    newButton.classList.add("btn");
+    newButton.classList.add("btn-primary")
+    newButton.addEventListener("click", async function()
     {
-      const newButtonContainer = document.createElement("span");
-      newButtonContainer.classList.add("buttoncontainer");
-      const newButton = document.createElement("button");
-      newButton.innerText = "Delete"
-      newButton.classList.add("btn");
-      newButton.classList.add("btn-primary")
-      newButton.addEventListener("click", async function()
-      {
-        const response = await fetch(`https://${window.location.hostname}/deleteCourse`, {
-          method:"POST",
-          headers:{
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-          },
-          body:JSON.stringify(data[index])
-        });
-        if(!response.ok)
-        {
-          console.log(response.error);
-          return;
-        }
-        else
-        {
-          console.log("POST sent successfully!");
-        }
+      const response = await fetch(`https://${window.location.hostname}/deleteCourse`, {
+        method:"POST",
+        headers:{
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body:JSON.stringify(data[index])
       });
-      newButtonContainer.appendChild(newButton);
-      newRow.appendChild(newButtonContainer);
-    }
+      if(!response.ok)
+      {
+        console.log(response.error);
+        return;
+      }
+      else
+      {
+        console.log("POST sent successfully!");
+      }
+    });
+    newButtonContainer.appendChild(newButton);
+    newRow.appendChild(newButtonContainer);
     document.getElementById("container").appendChild(newRow);
   }
 });
